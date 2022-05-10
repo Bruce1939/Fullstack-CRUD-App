6dfe1f78e88a5a86a71a88b62252920307bcfec1
Yash <yashpandey2002@gmail.com>
2022-05-10 16:15:09 +0530

    Third Commit

--- server/app.js
+++ server/app.js
@@ -34,10 +34,10 @@ app.use('/task', taskRoutes);
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../','client', 'build', 'index.html'));
   });
 }
 
