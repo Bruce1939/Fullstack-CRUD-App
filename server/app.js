3e1ce2fc1bb883ecd0385359d385278c84c4120f
Yash <yashpandey2002@gmail.com>
2022-05-10 16:00:52 +0530

    Second Commit

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -30,5 +31,14 @@ app.use(express.urlencoded());
 
 app.use('/auth', authRoutes);
 app.use('/task', taskRoutes);
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
